c410f45fc542d396b6a4d4ce7f35cbc36b40070e
Dane Guevara <dane.guevara@softvu.com>
2018-02-23 13:55:14 -0600

    Initialize graphql.

--- api/app.js
+++ api/app.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import graphql from 'express-graphql';
 import morgan from 'morgan';
+import schema from './graphql/schema';
 
 const app = express();
 const port = 6969;
 
 app.use(morgan('short'));
 
-app.all('/', (req, res) => {
-  res.send('hello');
-})
+app.use('/graphql', graphql({
+  schema,
+  graphiql: true,
+}));
 
 const start = () => {
   cs.info(`Starting dnd5e api on port ${port}`);
